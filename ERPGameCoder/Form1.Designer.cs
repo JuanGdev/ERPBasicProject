7e36aeb67b1a3a4504137251b67fb1b3f5631d81
Juan Aguilera <j.aguilerahuerta@ugto.mx>
2025-05-25 15:35:32 -0600

    Add project files.

---
+++ ERPGameCoder/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ERPGameCoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_usuarioId = new System.Windows.Forms.TextBox();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_password
+            // 
+            this.tb_password.AcceptsReturn = true;
+            this.tb_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_password.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.tb_password.Location = new System.Drawing.Point(350, 217);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(208, 38);
+            this.tb_password.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(189, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(211, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Usuario";
+            // 
+            // tb_usuarioId
+            // 
+            this.tb_usuarioId.AcceptsReturn = true;
+            this.tb_usuarioId.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_usuarioId.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.tb_usuarioId.Location = new System.Drawing.Point(350, 173);
+            this.tb_usuarioId.Name = "tb_usuarioId";
+            this.tb_usuarioId.PasswordChar = '*';
+            this.tb_usuarioId.Size = new System.Drawing.Size(208, 38);
+            this.tb_usuarioId.TabIndex = 2;
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btn_ingresar.Location = new System.Drawing.Point(314, 342);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(98, 37);
+            this.btn_ingresar.TabIndex = 4;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_ingresar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_usuarioId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_password);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_usuarioId;
+        private System.Windows.Forms.Button btn_ingresar;
+    }
+}
+
---
+++ ERPGameCoder/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace ERPGameCoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
+        {
+            // Obtener los valores ingresados por el usuario
+            string usuarioId = tb_usuarioId.Text;
+            string Email = tb_password.Text;
+
+            // Validar que los campos no estén vacíos
+            if (string.IsNullOrWhiteSpace(usuarioId) || string.IsNullOrWhiteSpace(Email))
+            {
+                MessageBox.Show("Por favor, ingresa el usuario y la contraseña.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Cadena de conexión desde App.config
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString;
+            // Consulta SQL para validar el usuario
+            string query = "SELECT COUNT(*) FROM Users WHERE Id = @Name";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@Name", usuarioId);
+                   // command.Parameters.AddWithValue("@Email", Email);
+
+                    connection.Open();
+                    int userCount = (int)command.ExecuteScalar();
+
+                    if (userCount > 0)
+                    {
+                        MessageBox.Show("¡Inicio de sesión exitoso!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña incorrectos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al conectar con la base de datos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
